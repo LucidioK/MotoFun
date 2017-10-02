8099f2cb72670e3f44a74f1db705b2e3cea548e8
Lucidio Kuhn <lucidiok@hotmail.com>
2017-10-01 23:12:22 -0700

    Refactored and simplified

--- MotoFun04/app/src/main/java/lucidiok/motofun04/MotoFunActivity.java
+++ MotoFun04/app/src/main/java/lucidiok/motofun04/MotoFunActivity.java
@@ -9,55 +9,42 @@ import android.widget.TextView;
 
 public class MotoFunActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener{
 
-    private TextureView mTextureView;
+    private TextureView     mTextureView;
     private CameraPreviewer mCameraPreviewer;
-    private TextView mSpeedView;
-    private Speedometer mSpeedometer;
+    private TextView        mSpeedView;
+    private Speedometer     mSpeedometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moto_fun);
-        //ConstraintLayout layout = (ConstraintLayout)findViewById(R.id.main);
-        mSpeedView = (TextView)findViewById(R.id.speed);
-        mSpeedView.setElevation(10);
-        //layout.addView(mSpeedView);
+        mSpeedView   = (TextView)findViewById(R.id.speed);
         mTextureView = (TextureView)findViewById(R.id.image);
         mTextureView.setSurfaceTextureListener(this);
-        mTextureView.setElevation(0);
     }
 
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
         try {
             mCameraPreviewer = new CameraPreviewer(this, mTextureView);
-            mSpeedometer = new Speedometer(this, mSpeedView);
+            mSpeedometer     = new Speedometer(this, mSpeedView);
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
-
-    }
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) { }
 
     @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
-        return true;
-    }
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) { return true; }
 
     @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
-
-    }
+    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) { }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (mSpeedometer != null){
-            mSpeedometer.Start();
-        }
         if (mCameraPreviewer != null) {
             try {
                 mCameraPreviewer.Start();
--- MotoFun04/app/src/main/java/lucidiok/motofun04/Speedometer.java
+++ MotoFun04/app/src/main/java/lucidiok/motofun04/Speedometer.java
@@ -7,11 +7,8 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.TextureView;
 import android.widget.TextView;
 
 /**
@@ -26,39 +23,25 @@ public class Speedometer  {
         public void onLocationChanged(Location location) {
             float speedMPH = location.getSpeed() / 2.236936f;
             int mph = (int)speedMPH;
-
             mTextView.setText(Integer.toString(mph));
         }
 
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-        }
-
-        public void onProviderEnabled(String provider) {
-        }
-
-        public void onProviderDisabled(String provider) {
-        }
-    };;
-
+        public void onStatusChanged(String provider, int status, Bundle extras) { }
+        public void onProviderEnabled(String provider) { }
+        public void onProviderDisabled(String provider) { }
+    };
 
     private static final int PERMISSION_REQUEST_CODE = 1000;
 
     public Speedometer(AppCompatActivity appCompatActivity, TextView textView) {
         mAppCompatActivity = appCompatActivity;
         mTextView = textView;
-        Start();
-    }
-
-    public void Start() {
-
+        //Start();
         if (ActivityCompat.checkSelfPermission(mAppCompatActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
             ActivityCompat.requestPermissions(mAppCompatActivity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
             return;
         }
         mLocationManager = (LocationManager)mAppCompatActivity.getSystemService(Context.LOCATION_SERVICE);
-
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
     }
-
-
 }
