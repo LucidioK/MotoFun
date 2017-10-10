fda35e52de5510c78c2e06c66e284f4ca6bdfcb4
Lucidio Kuhn <lucidiok@hotmail.com>
2017-10-10 10:14:40 -0700

    Showing directions, not tested

--- MotoFun04/app/src/main/java/lucidiok/motofun04/MapViewer.java
+++ MotoFun04/app/src/main/java/lucidiok/motofun04/MapViewer.java
@@ -6,6 +6,8 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +16,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.DirectionsApi;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsStep;
+
+import java.io.IOException;
 
 /**
  * Created by lucidiok on 10/3/17.
@@ -25,14 +34,22 @@ public class MapViewer implements OnMapReadyCallback {
     private MapFragment             mMapFragment;
     private GoogleMap               mGoogleMap;
     private LocationManager         mLocationManager;
+    private GeoApiContext           mGeoApiContext;
+    private LatLng                  mCurrentLatLong;
+    private DirectionsStep[]        mDirectionsSteps;
+    private TextView                mNextDirectionStep;
+
     protected LocationListener mLocationListener = new android.location.LocationListener() {
         public void onLocationChanged(Location location) {
 
-            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            CameraPosition pos = new CameraPosition(latLng, location.getBearing(), mGoogleMap.getCameraPosition().tilt, 15f/*mGoogleMap.getCameraPosition().zoom*/);
+            mCurrentLatLong = new LatLng(location.getLatitude(), location.getLongitude());
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(mCurrentLatLong));
+            CameraPosition pos = new CameraPosition(mCurrentLatLong, location.getBearing(), mGoogleMap.getCameraPosition().tilt, 15f/*mGoogleMap.getCameraPosition().zoom*/);
             mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(pos));
             mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(15f));
+            if (mDirectionsSteps != null){
+                displayNextStep();
+            }
         }
 
         public void onStatusChanged(String provider, int status, Bundle extras) { }
@@ -40,18 +57,48 @@ public class MapViewer implements OnMapReadyCallback {
         public void onProviderDisabled(String provider) { }
     };
 
-    public MapViewer(AppCompatActivity appCompatActivity, int mapFragmentId) {
+    private void displayNextStep() {
+        double mininumDistance = (double)Long.MAX_VALUE;
+        int nearestStep = -1;
+        for (int i = 0; i < mDirectionsSteps.length; i++) {
+            double dist = distance(mCurrentLatLong, new LatLng(mDirectionsSteps[0].endLocation.lat, mDirectionsSteps[0].endLocation.lng));
+            if (dist < mininumDistance) {
+                mininumDistance = dist;
+                nearestStep = i;
+            }
+        }
+        mNextDirectionStep.setVisibility(View.VISIBLE);
+        mNextDirectionStep.setText((CharSequence) mDirectionsSteps[nearestStep].htmlInstructions);
+    }
+
+    private double distance(LatLng p1, LatLng p2) {
+        double dx2 = Math.pow(Math.abs(p1.latitude - p2.latitude), 2);
+        double dy2 = Math.pow(Math.abs(p1.longitude - p2.longitude), 2);
+        return Math.sqrt(dx2 + dy2);
+    }
+
+    public MapViewer(AppCompatActivity appCompatActivity, int mapFragmentId, int nextDirectionStepId) {
         //super(appCompatActivity);
         mAppCompatActivity = appCompatActivity;
+        mNextDirectionStep = (TextView)mAppCompatActivity.findViewById(nextDirectionStepId);
         mMapFragment = (MapFragment) mAppCompatActivity.getFragmentManager()
                 .findFragmentById(mapFragmentId);
         mMapFragment.getMapAsync(this);
         mMapFragment.getView().setAlpha(0.5f);
     }
 
-    public void SetDestination(String destination){
-
+    public void StartDestination(String destination) throws InterruptedException, ApiException, IOException {
+        //GeoApiContext geoApiContext = new GeoApiContext();
+        final String origin = mCurrentLatLong.latitude + "," + mCurrentLatLong.longitude;
+        DirectionsResult result =
+            DirectionsApi.getDirections(mGeoApiContext, origin, destination).await();
+        mDirectionsSteps = result.routes[0].legs[0].steps;
     }
+
+    public void StopDestination(){
+        mDirectionsSteps = null;
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         try {
@@ -60,8 +107,11 @@ public class MapViewer implements OnMapReadyCallback {
             mGoogleMap.setTrafficEnabled(true);
             mGoogleMap.setBuildingsEnabled(false);
             mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(15f));
+            mGoogleMap.getUiSettings().setCompassEnabled(true);
+            mGoogleMap.getUiSettings().setMapToolbarEnabled(true);
             mLocationManager = (LocationManager)mAppCompatActivity.getSystemService(Context.LOCATION_SERVICE);
             mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 50, mLocationListener);
+            mGeoApiContext = new GeoApiContext.Builder().apiKey("AIzaSyD_iJCQ-XVvHJ9qryEycY3Scfb37DWYtg0").build();
         } catch (SecurityException e) {
             e.printStackTrace();
         }
--- MotoFun04/app/src/main/java/lucidiok/motofun04/MotoFunActivity.java
+++ MotoFun04/app/src/main/java/lucidiok/motofun04/MotoFunActivity.java
@@ -7,7 +7,12 @@ import android.view.KeyEvent;
 import android.view.TextureView;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.maps.errors.ApiException;
+
+import java.io.IOException;
 
 public class MotoFunActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
 
@@ -15,13 +20,25 @@ public class MotoFunActivity extends AppCompatActivity implements TextureView.Su
     private Speedometer     mSpeedometer;
     private MapViewer       mMapViewer;
     private TextView        mAdressText;
+    private TextView        mNextDirectionStepText;
+    private Button          mButDest;
     private View.OnKeyListener mAddressTextKeyListener = new View.OnKeyListener() {
 
         @Override
         public boolean onKey(View view, int i, KeyEvent keyEvent) {
             if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
                 mAdressText.setVisibility(View.INVISIBLE);
+                mNextDirectionStepText.setVisibility(View.VISIBLE);
                 mInputMethodManager.hideSoftInputFromWindow(mAdressText.getWindowToken(), 0);
+                try {
+                    mMapViewer.StartDestination(mAdressText.getText().toString());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ApiException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 return true;
             }
             return false;
@@ -36,6 +53,8 @@ public class MotoFunActivity extends AppCompatActivity implements TextureView.Su
         ((TextureView)findViewById(R.id.image)).setSurfaceTextureListener(this);
         mAdressText = (TextView)findViewById(R.id.address);
         mAdressText.setOnKeyListener(mAddressTextKeyListener);
+        mNextDirectionStepText = (TextView)findViewById(R.id.nextDirectionStep);
+        mButDest = (Button)findViewById(R.id.butDest);
     }
 
     @Override
@@ -43,7 +62,7 @@ public class MotoFunActivity extends AppCompatActivity implements TextureView.Su
         try {
             mCameraPreviewer = new CameraPreviewer(this, R.id.image);
             mSpeedometer     = new Speedometer(this, R.id.speed);
-            mMapViewer       = new MapViewer(this, R.id.map);
+            mMapViewer       = new MapViewer(this, R.id.map, R.id.nextDirectionStep);
             mInputMethodManager = (InputMethodManager)getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
         } catch (Exception e) {
             e.printStackTrace();
@@ -60,7 +79,16 @@ public class MotoFunActivity extends AppCompatActivity implements TextureView.Su
     public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) { }
 
     public void onClickDest(View v) {
-        mAdressText.setVisibility(View.VISIBLE);
-        mInputMethodManager.toggleSoftInputFromWindow(mAdressText.getApplicationWindowToken(), InputMethodManager.SHOW_FORCED, 0);
+        String buttonText = ((Button)findViewById(R.id.butDest)).getText().toString();
+        String doneString = getString(R.string.done);
+        if (buttonText == doneString) {
+            mNextDirectionStepText.setVisibility(View.INVISIBLE);
+            mButDest.setText(R.string.dest);
+
+        } else {
+            mAdressText.setVisibility(View.VISIBLE);
+            mInputMethodManager.toggleSoftInputFromWindow(mAdressText.getApplicationWindowToken(), InputMethodManager.SHOW_FORCED, 0);
+            mButDest.setText(R.string.done);
+        }
     }
 }
